8afe730c0ca117c8ba3a00aa00c00fa4e8c20ffd
janhavisakoji <janhavisakoji@gmail.com>
2026-08-10 20:20:31 +0530

    Add Day 01 positive, negative and zero

---
+++ Day_01/problem-02.js
@@ -0,0 +1,15 @@
+// check if the number is positive, negative or zero.
+
+// input = -18
+// output = -18 is Negative
+
+let number = -18;
+if (number > 0) {
+    console.log(number, "is positive");
+}
+else if (number < 0) {
+    console.log(number, "is Negative");
+}
+else {
+    console.log(number, "is Zero");
+}
