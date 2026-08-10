800b6cb8444f82b0b6d72802c127249a3c31d88a
janhavisakoji <janhavisakoji@gmail.com>
2026-08-11 02:00:23 +0530

    print 1 to 10 using while loop

---
+++ Day_02/problem-01.js
@@ -0,0 +1,10 @@
+// Write a while loop that prints the numbers from 1 to 10.
+// input: none
+// output: 1 2 3 4 5 6 7 8 9 10
+
+let num = 1;
+
+while (num <= 10) {
+    console.log(num);
+    num++;
+}
