b2e38e4d742cdfc7b3341ae1374acec2dc1f2c56
janhavisakoji <janhavisakoji@gmail.com>
2026-08-10 23:36:32 +0530

    Add Day 01 character is uppercase, lowercase, number or special character

---
+++ Day_01/problem-07.js
@@ -0,0 +1,15 @@
+// Check whether a character is uppercase, lowercase, a number, or a special character.
+let char = "#";
+
+if (char >= 'A' && char <= 'Z') {
+    console.log(char + " is an uppercase letter.");
+}
+else if (char >= 'a' && char <= 'z') {
+    console.log(char + " is a lowercase letter.");
+}
+else if (char >= '0' && char <= '9') {
+    console.log(char + " is a number.");
+}
+else {
+    console.log(char + " is a special character.");
+}
