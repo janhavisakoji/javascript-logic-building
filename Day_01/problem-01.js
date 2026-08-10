9302fec32b9a784279800a7c65152a34ace67b62
janhavisakoji <janhavisakoji@gmail.com>
2026-08-10 19:32:40 +0530

    Add Day 01 even and odd number

---
+++ Day_01/problem-01.js
@@ -0,0 +1,10 @@
+//  check weather a number is even or odd
+/* input: 19
+output: 19 is odd number */
+
+let number = 19;
+if (number % 2 === 0) {
+    console.log(number, "is even number");
+} else {
+    console.log(number, "is odd number");
+}
