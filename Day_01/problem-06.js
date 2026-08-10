6b7af672ac953ebdba260f1f5d8dccee2f8b886c
janhavisakoji <janhavisakoji@gmail.com>
2026-08-10 21:17:48 +0530

    Add Day 01 vowel or consonent

---
+++ Day_01/problem-06.js
@@ -0,0 +1,12 @@
+// Write a program that checks whether a given character is a vowel or a consonant.
+//input: e 
+//output: e is a vowel
+
+let character = 'e';
+
+if (character === 'a' || character === 'e' || character === 'i' || character === 'o' || character === 'u') {
+    console.log(character + " is a vowel.");
+}
+else {
+    console.log(character + " is a consonant.");
+}
