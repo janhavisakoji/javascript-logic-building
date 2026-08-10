430ba4b7aba09e9da013ef255c19189cfc1e3436
janhavisakoji <janhavisakoji@gmail.com>
2026-08-11 02:14:23 +0530

    print numbers from 10 to 1

---
+++ Day_02/problem-02.js
@@ -0,0 +1,10 @@
+// Write a while loop that prints the numbers from 10 to 1.
+// input: 10
+// output: 10 9 8 7 6 5 4 3 2 1
+
+let num = 10;
+
+while (num >= 1) {
+    console.log(num);
+    num--;
+}
