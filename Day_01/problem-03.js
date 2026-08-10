72b0902350d15e59d8f16a1b4ae469e57bc23d72
janhavisakoji <janhavisakoji@gmail.com>
2026-08-10 20:54:38 +0530

    Add Day 01 larger number between two numbers

---
+++ Day_01/problem-03.js
@@ -0,0 +1,13 @@
+// Find the larger number between two numbers.
+// input = 108, 299
+// output = 299 is larger number
+
+let num1 = 108;
+let num2 = 299;
+
+if (num1 > num2) {
+    console.log(num1, "is larger number");
+}
+else {
+    console.log(num2, "is larger number");
+}
