071c970ae0c25f3ff64b643f69422de587debfca
janhavisakoji <janhavisakoji@gmail.com>
2026-08-10 23:42:20 +0530

    Add Day 01 vote eligibility

--- Day_01/problem-08.js
+++ Day_01/problem-08.js
@@ -1,9 +1,9 @@
 // Write a program to check if a person is eligible to vote based on their age. If the age is 18 or above, print "You are eligible to vote." Otherwise, print "You are not eligible to vote."
-//input: 20
+//input: 24
 //output: You are eligible to vote.
 
 
-let age = 20;
+let age = 24;
 
 if (age >= 18) {
     console.log("You are eligible to vote.");
